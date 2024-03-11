059c969df8ec4b6cc9627285d0884e8ffe14a1b8
QuiLion7 <quilbrub@gmail.com>
2024-03-11 01:25:26 -0300

    refactor(page): Remove UserType component from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,4 @@
-import UserType from "@/components/userType";
 
 export default function Home() {
-  return (
-    <main className="p-2">
-      Página Inicial
-      <UserType />
-    </main>
-  );
+  return <main className="p-2">Página Inicial</main>;
 }
