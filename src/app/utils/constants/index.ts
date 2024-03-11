fa04b82e738d66828ca53e7821067b9c0e8cf96d
QuiLion7 <quilbrub@gmail.com>
2024-03-11 01:25:19 -0300

    feat(utils): Create constants file to store categories

---
+++ src/app/utils/constants/index.ts
@@ -0,0 +1,11 @@
+export const categories: { id: number; name: string }[] = [
+  { id: 1, name: "Accommodation" },
+  { id: 2, name: "Transportation" },
+  { id: 3, name: "Food" },
+  { id: 4, name: "Leisure Activities" },
+  { id: 5, name: "Entertainment" },
+  { id: 6, name: "Travel Guides and Tours" },
+  { id: 7, name: "Spa and Wellness Services" },
+  { id: 8, name: "Adventure and Ecotourism" },
+  { id: 9, name: "Alternative Transportation" },
+];
