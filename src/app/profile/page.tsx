784d36b7ce547532b3068fc43a035f370f4c02db
QuiLion7 <quilbrub@gmail.com>
2024-03-11 16:35:44 -0300

    feat: Restricted access to Profile page to logged-in users only

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,12 @@
-export default function Profile() {
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+export default async function Profile() {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    redirect("/");
+  }
   return <main className="p-2">Perfil do Usuário</main>;
 }
