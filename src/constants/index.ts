651725656c360006ad2ee1e87ff5487532124169
QuiLion7 <quilbrub@gmail.com>
2024-03-13 03:29:10 -0300

    feat: Added 'url' property to categories constant and Added establishmentTypes and services constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -10,12 +10,74 @@ import {
 } from "lucide-react";
 
 export const categories = [
-  { id: 1, name: "Accommodation", icon: BedDouble },
-  { id: 2, name: "Transportation", icon: Car },
-  { id: 3, name: "Good Food", icon: Utensils },
-  { id: 4, name: "Entertainment", icon: Drama },
+  { id: 1, name: "Accommodation", icon: BedDouble, url: "accommodation" },
+  { id: 2, name: "Transportation", icon: Car, url: "transportation" },
+  { id: 3, name: "Good Food", icon: Utensils, url: "food" },
+  { id: 4, name: "Entertainment", icon: Drama, url: "entertainment" },
   // { id: 5, name: "Leisure Activities", icon: TreePalm },
   // { id: 6, name: "Travel Guides and Tours", icon: MapPinned },
   // { id: 7, name: "Adventure and Ecotourism", icon: Turtle },
   // { id: 8, name: "Alternative Transportation", icon: Bike },
+];
+
+export const establishmentTypes = [
+  "Apartment",
+  "Bungalow",
+  "Home",
+  "Chalet",
+  "Room",
+];
+export const services = [
+  {
+    id: "wifi",
+    label: "Wi-Fi",
+  },
+  {
+    id: "breakfast",
+    label: "Breakfast",
+  },
+  {
+    id: "parking",
+    label: "Parking",
+  },
+  {
+    id: "pool",
+    label: "Swimming Pool",
+  },
+  {
+    id: "gym",
+    label: "Gym",
+  },
+  {
+    id: "air_conditioning",
+    label: "Air Conditioning",
+  },
+  {
+    id: "tv",
+    label: "TV",
+  },
+  {
+    id: "laundry",
+    label: "Laundry",
+  },
+  {
+    id: "room_service",
+    label: "Room Service",
+  },
+  {
+    id: "beach_access",
+    label: "Beach Access",
+  },
+  {
+    id: "restaurant",
+    label: "Restaurant",
+  },
+  {
+    id: "bar",
+    label: "Bar",
+  },
+  {
+    id: "bbq_facilities",
+    label: "BBQ Facilities",
+  },
 ];
