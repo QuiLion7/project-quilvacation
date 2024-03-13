7f7471c33b2ad69aeae660360a1452b0345f3594
QuiLion7 <quilbrub@gmail.com>
2024-03-13 11:33:08 -0300

    feat: Added Firebase storage connection

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -13,5 +13,7 @@ const firebaseConfig = {
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const storage = getStorage(app);
+const db = getFirestore(app);
 
-export const db = getFirestore(app);
+export { db, storage };
