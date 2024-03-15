bb0f033eae49456767da67283cf2e402e1c01f3a
QuiLion7 <quilbrub@gmail.com>
2024-03-15 03:18:04 -0300

    feat: Added Manage component to create page

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,6 +1,7 @@
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { Manage } from "./components/manage";
 
 export default async function CreateOffer() {
   const session = await getServerSession(authOptions);
@@ -8,6 +9,13 @@ export default async function CreateOffer() {
   if (!session || !session.user) {
     redirect("/");
   }
-  
-  return <main className="p-2">Criar Ofertas</main>;
+
+  return (
+    <main className="flex h-full w-full flex-col items-center justify-center p-2">
+      <h1 className="uppercase font-bold text-sm sm:text-base md:text-lg lg:text-2xl my-5">
+        to manage
+      </h1>
+      <Manage />
+    </main>
+  );
 }
