fc436608eb6cc1cc95573bd49b83fefcf5dcd0c9
QuiLion7 <quilbrub@gmail.com>
2024-03-19 01:32:05 -0300

    refactor: Renamed component from ShowConfig to ShowConfigCreate

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,7 +1,7 @@
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { ShowConfig } from "./components/showConfig";
+import { ShowConfigCreate } from "./components/showConfigCreate";
 
 export default async function CreateOffer() {
   const session = await getServerSession(authOptions);
@@ -12,7 +12,7 @@ export default async function CreateOffer() {
 
   return (
     <main className="mt-[57px] max-w-7xl flex h-full w-full flex-col items-center justify-center p-2">
-      <ShowConfig />
+      <ShowConfigCreate />
     </main>
   );
 }
