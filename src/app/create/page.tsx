c7d660fcaa27b6c4a2f7e080a4459e44c68dc994
QuiLion7 <quilbrub@gmail.com>
2024-03-18 22:31:51 -0300

    feat: Added the ShowConfig component to the Create page

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,7 +1,7 @@
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-import { Manage } from "./components/manage";
+import { ShowConfig } from "./components/showConfig";
 
 export default async function CreateOffer() {
   const session = await getServerSession(authOptions);
@@ -11,11 +11,8 @@ export default async function CreateOffer() {
   }
 
   return (
-    <main className="flex h-full w-full flex-col items-center justify-center p-2">
-      <h1 className="uppercase font-bold text-sm sm:text-base md:text-lg lg:text-2xl my-5">
-        to manage
-      </h1>
-      <Manage />
+    <main className="mt-[57px] max-w-7xl flex h-full w-full flex-col items-center justify-center p-2">
+      <ShowConfig />
     </main>
   );
 }
