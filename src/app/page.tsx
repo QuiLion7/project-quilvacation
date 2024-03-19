634e9db672843c78509822e60b9a18f96a96ba20
QuiLion7 <quilbrub@gmail.com>
2024-03-18 22:22:54 -0300

    feat: Split the Home page into components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,17 @@
+import HomeButtons from "@/components/homeButtons";
 import { ShowItems } from "@/components/showItems";
-import { categories } from "@/constants";
+import TitleAnimation from "@/components/titleAnimated";
 
 export default function Home() {
   return (
-    <main className="p-2 max-w-7xl w-full flex flex-col justify-center items-center gap-2">
-      <div className="w-full flex justify-center items-center bg-[url('/card2.jpg')] bg-cover bg-no-repeat bg-center bg-origin-border h-[350px] md:h-[500px] rounded-xl mb-24">
-        <div className="relative flex flex-col justify-center items-center bg-primary/20 hover:bg-primary/0 duration-300 w-full h-full rounded-xl">
-          <h1 className="font-bold text-center text-xl sm:text-3xl md:text-4xl lg:text-5xl uppercase p-2 bg-secondary/80 hover:scale-110 duration-500 rounded-xl tracking-wide cursor-default">
-            holidays <br />
-            await you
-          </h1>
-          <ul className="absolute bottom-[-80px] sm:bottom-[-45px] md:bottom-[-50px] w-full h-auto flex gap-1 md:gap-2 justify-center lg:justify-evenly items-center rounded-xl p-2 flex-wrap">
-            {categories.map((category) => (
-              <li
-                key={category.id}
-                className="w-[140px] sm:w-[150px] md:w-[180px] lg:w-[220px] px-4 bg-secondary/80 rounded-xl flex flex-col justify-evenly items-center cursor-default hover:scale-105
-                duration-300 hover:font-bold"
-              >
-                <div className="flex-1">
-                  <category.icon className="w-[30px] sm:w-[35px] md:w-[40px] lg:w-[45px] h-auto py-1 md:py-2 " />
-                </div>
-                <p className="flex justify-center items-center text-center flex-1 uppercase text-xs sm:text-sm md:text-base lg:text-xl py-2 ">
-                  {category.name}
-                </p>
-              </li>
-            ))}
-          </ul>
+    <main className="mt-[57px] max-w-7xl w-full flex flex-col justify-center items-center gap-2 p-2 rounded-xl">
+      <div className="w-full flex justify-center items-center bg-[url('/bg.jpg')] bg-cover bg-no-repeat bg-center h-[350px] md:h-[530px] rounded-xl">
+        <div className="flex justify-center items-center w-full h-full">
+          <TitleAnimation />
         </div>
       </div>
-      <div className="flex h-full w-full flex-col items-center justify-center ">
-        <h1 className="uppercase font-bold text-sm sm:text-base md:text-lg lg:text-2xl my-5">
-          Most accessed
-        </h1>
+      <HomeButtons />
+      <div className="flex h-full w-full flex-col items-center justify-center mb-10">
         <ShowItems />
       </div>
     </main>
