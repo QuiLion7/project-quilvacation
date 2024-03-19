ce7dd1d83cc333428746dc3ae255a75b0147af21
QuiLion7 <quilbrub@gmail.com>
2024-03-19 01:28:25 -0300

    feat: Created LoadingDataVacation component

---
+++ src/app/vacation/components/loadingDataVacation/index.tsx
@@ -0,0 +1,217 @@
+"use client";
+
+import { db, storage } from "@/lib/firebase";
+import {
+  collection,
+  query,
+  getDocs,
+  orderBy,
+  where,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { format } from "date-fns";
+import { Trash2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useSession } from "next-auth/react";
+import { UserIDProps } from "@/lib/auth";
+import { deleteObject, ref } from "firebase/storage";
+import { Card, CardContent } from "@/components/ui/card";
+import Link from "next/link";
+
+interface ListVacationProps {
+  id: string;
+  type: string;
+  establishmentName: string;
+  price: string | number;
+  servicesIncluded: string[];
+  photos: ListImageProps[];
+  description: string;
+  dateRange: {
+    from: TimestampProps;
+    to: TimestampProps;
+  };
+  state: string;
+  city: string;
+  address: string;
+  whatsapp: string;
+  owner: string;
+  uid: string;
+  createType: string;
+}
+
+interface ItemVacationProps {
+  id: string;
+  uid: string;
+}
+
+interface ListImageProps {
+  name: string;
+  uid: string;
+  url: string;
+}
+
+interface TimestampProps {
+  seconds: number;
+  nanoseconds: number;
+}
+
+export function LoadingDataVacation() {
+  const { data } = useSession();
+  const user: UserIDProps | undefined = data?.user;
+  const userID = user?.id;
+
+  const [listVacation, setListVacation] = useState<ItemVacationProps[]>([]);
+
+  useEffect(() => {
+    const loadList = async () => {
+      if (!userID) {
+        console.error("User ID not found.");
+        return;
+      }
+      try {
+        const listRef = collection(db, "vacation");
+        const queryRef = query(
+          listRef,
+          where("uid", "==", userID),
+          orderBy("created", "desc")
+        );
+        const snapshot = await getDocs(queryRef);
+
+        let listManage = [] as ItemVacationProps[];
+
+        snapshot.forEach((doc) => {
+          listManage.push({
+            id: doc.id,
+            uid: doc.data().uid,
+          });
+        });
+        console.log(listManage);
+        setListVacation(listManage);
+      } catch (error) {
+        console.error("Error when searching for item:", error);
+        toast.error("Error when searching for item");
+      }
+    };
+
+    loadList();
+  }, [userID]);
+
+  function timestampToDate(timestamp: TimestampProps) {
+    const date = new Date(timestamp.seconds * 1000);
+    return date;
+  }
+
+  async function handleDeleteItem(establishment: ListVacationProps) {
+    const docRef = doc(db, "manage", establishment.id);
+
+    establishment.photos.map(async (photo) => {
+      const imagePath = `images/${photo.uid}/${photo.name}`;
+      const imageRef = ref(storage, imagePath);
+
+      try {
+        await deleteDoc(docRef);
+        await deleteObject(imageRef);
+        setListVacation(
+          listVacation.filter((item) => item.id !== establishment.id)
+        );
+        toast.success("Deleted Item");
+      } catch (error) {
+        console.error("Error delete:", error);
+        toast.error("Error delete");
+      }
+    });
+  }
+
+  return (
+    <main className="w-full flex flex-col justify-center items-center">
+      {listVacation.length === 0 && (
+        <div className="w-full h-[65vh] uppercase flex flex-col justify-center items-center text-primary">
+          <h1 className="font-bold text-base sm:text-lg md:text-xl lg:text-2xl">
+            no offers registered
+          </h1>
+          <h3 className="font-bold text-xs sm:text-sm md:text-lg lg:text-xl">
+            click on any form to get started
+          </h3>
+        </div>
+      )}
+      {listVacation.length > 0 && (
+        <h1 className="uppercase font-bold text-sm sm:text-base md:text-lg lg:text-2xl my-5">
+          to manage
+        </h1>
+      )}
+
+      {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 w-auto">
+        {listVacation.map((item) => (
+          <Card
+            key={item.id}
+            className="rounded-xl bg-primary/80 hover:bg-primary duration-300 w-full h-full"
+          >
+            <CardContent className="relative group flex flex-col w-[300px] h-[350px] sm:w-[220px] sm:h-[340px] md:w-[240px] md:h-[360px] aspect-square items-center justify-center rounded-xl text-secondary p-0 ">
+              <div className="flex justify-center items-center w-full  h-full rounded-xl">
+                <Button
+                  className="absolute z-10 top-0 right-[-8px] bg-transparent hover:bg-primary/0 duration-300 w-auto h-auto flex justify-center items-center drop-shadow"
+                  onClick={() => handleDeleteItem(item)}
+                >
+                  <Trash2 className="text-primary bg-secondary p-1 rounded-xl w-[30px] h-auto cursor-pointer hover:scale-110 duration-300" />
+                </Button>
+                <Link href={`/offers/${item.id}`}>
+                  <Image
+                    src={item.photos[0].url}
+                    alt={item.createType}
+                    layout="responsive"
+                    quality={100}
+                    unoptimized={true}
+                    width={0}
+                    height={0}
+                    style={{
+                      height: "200px",
+                      width: "300px",
+                      objectFit: "cover",
+                    }}
+                    className="center w-auto h-full object-cover rounded-xl group-hover:scale-95 duration-300 cursor-pointer"
+                  />
+                </Link>
+              </div>
+              <div className="flex text-sm md:text-base flex-col gap-1 justify-center items-center p-2 w-full sm:w-full h-full uppercase group-hover:scale-105 duration-300">
+                <p className="hidden sm:block font-bold text-center">
+                  {item.createType}
+                </p>
+                <p className="cursor-default text-center">{item.type}</p>
+                <span className="cursor-default text-center">
+                  {item.city} / {item.state}
+                </span>
+                <span className="cursor-default hover:scale-105 duration-300">
+                  from:{" "}
+                  {format(timestampToDate(item.dateRange.from), "MM/dd/yyyy")}
+                </span>
+                <span className="cursor-default hover:scale-105 duration-300">
+                  to: {format(timestampToDate(item.dateRange.to), "MM/dd/yyyy")}
+                </span>
+                <span className="cursor-default font-bold">
+                  Price: $ {item.price}
+                </span>
+              </div>
+            </CardContent>
+          </Card>
+        ))}
+
+        <ToastContainer
+          position="bottom-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </div> */}
+    </main>
+  );
+}
