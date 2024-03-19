2b270a2d41268ae6adc84f4aab01cba67de9e381
QuiLion7 <quilbrub@gmail.com>
2024-03-18 22:38:28 -0300

    refactor: Moved NavDesktop and NavMobile components to the header component folder

--- src/components/header/components/navDesktop/index.tsx
+++ src/components/header/components/navDesktop/index.tsx
@@ -5,7 +5,9 @@ import { Button } from "@/components/ui/button";
 import { HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
 import UserType from "@/components/userType";
 import { UserContext } from "@/contexts/UserContext";
+import { fadeIn } from "@/utils/variants";
 import { HoverCard } from "@radix-ui/react-hover-card";
+import { motion } from "framer-motion";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -26,16 +28,28 @@ const NavDesktop = () => {
         href="/"
         className="h-[45px] w-full flex justify-start items-center"
       >
-        <h1 className="font-bold text-sm md:text-xl lg:text-2xl xl:text-3xl">
+        <motion.h1
+          variants={fadeIn("left", 0.7)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.7 }}
+          className="font-bold text-sm md:text-xl lg:text-2xl xl:text-3xl"
+        >
           QUIL
           <span className="bg-gradient-to-r from-purple-900 to-indigo-500 bg-clip-text text-transparent">
             VACATION
           </span>
-        </h1>
+        </motion.h1>
       </Link>
 
       {status === "authenticated" && userType === "client" && (
-        <ul className="flex flex-1 h-[45px] w-full items-center justify-end gap-3 text-sm uppercase">
+        <motion.ul
+          variants={fadeIn("right", 0.4)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.7 }}
+          className="flex flex-1 h-[45px] w-full items-center justify-end gap-3 text-sm uppercase"
+        >
           <li
             className={`${
               pathname === "/"
@@ -47,7 +61,7 @@ const NavDesktop = () => {
           </li>
           <li
             className={`${
-              pathname === "/offers"
+              beforePathName === "offers"
                 ? "bg-primary text-secondary p-2 rounded-lg hover:bg-secondary hover:text-primary duration-300 font-bold"
                 : "hover:bg-primary hover:text-secondary p-2 rounded-lg duration-300"
             }`}
@@ -90,11 +104,17 @@ const NavDesktop = () => {
           >
             <Link href="/contact">Contact</Link>
           </li>
-        </ul>
+        </motion.ul>
       )}
 
       {status === "authenticated" && userType === "advertiser" && (
-        <ul className="flex flex-1 h-[45px] w-full items-center justify-end gap-3 text-sm uppercase">
+        <motion.ul
+          variants={fadeIn("right", 0.4)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.7 }}
+          className="flex flex-1 h-[45px] w-full items-center justify-end gap-3 text-sm uppercase"
+        >
           <li
             className={`${
               pathname === "/"
@@ -115,7 +135,7 @@ const NavDesktop = () => {
           </li>
           <li
             className={`${
-              pathname === "/offers"
+              beforePathName === "offers"
                 ? "bg-primary text-secondary p-2 rounded-lg hover:bg-secondary hover:text-primary duration-300 font-bold"
                 : "hover:bg-primary hover:text-secondary p-2 rounded-lg duration-300"
             }`}
@@ -166,11 +186,17 @@ const NavDesktop = () => {
               Logout
             </HoverCardContent>
           </HoverCard>
-        </ul>
+        </motion.ul>
       )}
 
       {status === "unauthenticated" && (
-        <ul className="flex flex-1 h-[45px] w-full items-center justify-end gap-3 text-sm uppercase">
+        <motion.ul
+          variants={fadeIn("right", 0.4)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.7 }}
+          className="flex flex-1 h-[45px] w-full items-center justify-end gap-3 text-sm uppercase"
+        >
           <li
             className={`${
               pathname === "/"
@@ -205,7 +231,7 @@ const NavDesktop = () => {
           >
             Login
           </Button>
-        </ul>
+        </motion.ul>
       )}
     </div>
   );
