7ece51c1d10721c32a13704ac824e0cdc0c3c38f
QuiLion7 <quilbrub@gmail.com>
2024-03-19 01:27:38 -0300

    refactor: Renamed component to ShowConfigCreate

--- src/app/vacation/page.tsx
+++ src/app/vacation/page.tsx
@@ -1,6 +1,7 @@
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { LoadingDataVacation } from "./components/loadingDataVacation";
 
 export default async function MyVacation() {
   const session = await getServerSession(authOptions);
@@ -8,7 +9,9 @@ export default async function MyVacation() {
   if (!session || !session.user) {
     redirect("/");
   }
-  return <main className="mt-[57px] p-2 max-w-7xl"><h1 className="uppercase font-bold text-sm sm:text-base md:text-lg lg:text-2xl my-5">
-  vacation under construction
-        </h1></main>;
+  return (
+    <main className="mt-[57px] p-2 max-w-7xl">
+      <LoadingDataVacation />
+    </main>
+  );
 }
