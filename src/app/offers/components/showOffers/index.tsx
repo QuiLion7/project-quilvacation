fefa58845b796dd70247b85e166c2c745d06712f
QuiLion7 <quilbrub@gmail.com>
2024-03-19 01:29:58 -0300

    feat: Updated handleAddItemCart function

--- src/app/offers/components/showOffers/index.tsx
+++ src/app/offers/components/showOffers/index.tsx
@@ -3,7 +3,14 @@
 import { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
 import { db } from "@/lib/firebase";
 import { Card, CardContent } from "@/components/ui/card";
 import Link from "next/link";
@@ -11,7 +18,9 @@ import Image from "next/image";
 import { format } from "date-fns";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Search, ShoppingCart } from "lucide-react";
+import { Search, SmilePlus } from "lucide-react";
+import { useSession } from "next-auth/react";
+import { UserIDProps } from "@/lib/auth";
 
 interface ListProps {
   type: string;
@@ -45,6 +54,10 @@ interface TimestampProps {
 }
 
 export function ShowOffers() {
+  const { data } = useSession();
+  const user: UserIDProps | undefined = data?.user;
+  const userID = user?.id;
+
   const [list, setList] = useState<ListProps[]>([]);
   const [input, setInput] = useState("");
 
@@ -145,9 +158,25 @@ export function ShowOffers() {
     }
   };
 
-  const handleAddItemCart = (item: string) => {
-    toast.info(`${item} unregistered... cart under construction`);
-  };
+  async function handleAddItemVacation(item: string) {
+    if (!userID) {
+      console.error("User ID not found.");
+      return;
+    }
+
+    try {
+      addDoc(collection(db, "vacation"), {
+        id: item,
+        uid: userID,
+        created: new Date(),
+      });
+
+      toast.success(`Adding To Vacation`);
+    } catch (error) {
+      console.log("Error add Vacation:", error);
+      toast.error("Error add Vacation");
+    }
+  }
 
   return (
     <div className="w-full flex flex-col gap-2 justify-center items-center">
@@ -155,18 +184,18 @@ export function ShowOffers() {
         <h1 className="uppercase font-bold text-sm sm:text-base md:text-lg lg:text-2xl bg-blend-exclusion">
           choose for you
         </h1>
-        <div className="w-full md:max-w-[80%] flex items-center justify-center bg-primary rounded-full p-2 relative">
+        <div className="w-full md:max-w-[80%] flex items-center justify-center bg-transparent rounded-full p-2 relative ">
           <Input
             type="text"
-            placeholder="search by desired category"
-            className="rounded-full bg-secondary"
+            placeholder="Search By Desired Category"
+            className="rounded-full bg-transparent border-2 border-primary"
             value={input}
             onChange={handleInputChange}
             onKeyDown={handleKeyDown}
           />
           <Button
             onClick={handleSearchItem}
-            className="absolute group right-2 rounded-r-full bg-secondary text-primary hover:bg-secondary"
+            className="absolute group right-2 rounded-r-full bg-transparent text-primary hover:bg-transparent"
           >
             <Search
               size={0}
@@ -183,10 +212,10 @@ export function ShowOffers() {
               <CardContent className="relative group flex flex-col w-[150px] h-[210px] sm:w-[220px] sm:h-[340px] md:w-[240px] md:h-[360px] aspect-square items-center justify-center rounded-xl text-secondary p-0 ">
                 <div className="flex justify-center items-center w-full  h-full rounded-xl">
                   <Button
-                    className="absolute z-10 top-0 left-[-8px] bg-transparent hover:bg-primary/0 duration-300 w-auto h-auto flex justify-center items-center drop-shadow"
-                    onClick={() => handleAddItemCart(item.type)}
+                    className="absolute z-10 top-0 right-[-8px] bg-transparent hover:bg-primary/0 duration-300 w-auto h-auto flex justify-center items-center drop-shadow"
+                    onClick={() => handleAddItemVacation(item.id)}
                   >
-                    <ShoppingCart className="text-primary bg-secondary p-1 rounded-xl w-[40px] h-auto cursor-pointer hover:scale-110 duration-300" />
+                    <SmilePlus className="text-primary bg-secondary p-1 rounded-xl w-[40px] h-auto cursor-pointer hover:scale-110 duration-300" />
                   </Button>
                   <Link href={`/offers/${item.id}`} className="block sm:hidden">
                     <Image
