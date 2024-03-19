2f9cab092eadf2ba86891b291ef4f655e60ba33b
QuiLion7 <quilbrub@gmail.com>
2024-03-18 22:27:46 -0300

    feat: Adjusted the responsiveness of the Contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,8 @@
-import FormContact from "@/components/formContact";
+import FormContact from "@/app/contact/components/formContact";
 
 export default function Contact() {
   return (
-    <main className="flex h-full w-full flex-col items-center justify-center p-2">
+    <main className="mt-[57px] max-w-7xl flex h-full w-full flex-col items-center justify-center p-2">
       <h1 className="uppercase font-bold text-sm sm:text-base md:text-lg lg:text-2xl my-5">
         Contact
       </h1>
